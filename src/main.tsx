03647096c4d242cf94addb81d31fde8b6acd5ca7
ThangHH <thanghh@fpt.com>
2026-02-27 00:13:45 +0700

    feat: Configure Monaco Editor to use a locally-bundled version and update Nginx CSP to allow Cloudflare insights and Google Fonts.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { loader } from '@monaco-editor/react'
+import * as monaco from 'monaco-editor'
 import App from './App.tsx'
+
+// Use the locally-bundled Monaco instead of loading from cdn.jsdelivr.net
+loader.config({ monaco })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
